b4e0e74ab7349d3ac968f3b4cdaae51d155a21f7
Ivan <ivan.ipv3@gmail.com>
2019-10-03 13:44:38 +0200

    Se mostrara el año de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 		System.out.println("Ivan Perez Vivas");
 		System.out.println("Soy de Leon");
 		System.out.println("Mi localidad actual seria Navatejera");
-
+		System.out.println("Mi año de nacimiento es el 13 de julio del 1992");
 
 	}
 }
